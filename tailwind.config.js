e85355f757b0e5b9e52122704d102d93abca87b6
Marc Jenald Galino <galinomarcjenald20@gmail.com>
2024-07-08 00:45:50 +0800

    Install daisyui package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -21,5 +22,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, daisyui],
 };
