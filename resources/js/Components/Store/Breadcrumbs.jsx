f094335dd4306b94f9ddff6d42b78743800e16cc
Marc Jenald Galino <galinomarcjenald20@gmail.com>
2024-07-10 02:16:24 +0800

    minor fix for breadcrumbs

--- resources/js/Components/Store/Breadcrumbs.jsx
+++ resources/js/Components/Store/Breadcrumbs.jsx
@@ -4,26 +4,28 @@ import { Link } from "@inertiajs/react";
 
 const Breadcrumbs = ({ data }) => {
     return (
-        <Container className="flex flex-wrap gap-2 py-10 uppercase font-light">
-            <span>You are here:</span>
-            {data &&
-                data.map((item) => (
-                    <>
-                        <span>
-                            <Link
-                                href={item.url}
-                                className={
-                                    route().current(item.route)
-                                        ? "font-bold"
-                                        : "font-light"
-                                }
-                            >
-                                {item.label}
-                            </Link>
-                        </span>
-                        {item.separator && <span>/</span>}
-                    </>
-                ))}
+        <Container className="flex items-center flex-wrap gap-2 py-10 uppercase font-light">
+            <div>You are here:</div>
+
+            <div className="breadcrumbs">
+                <ul>
+                    {data &&
+                        data.map((item) => (
+                            <li>
+                                <Link
+                                    href={item.url}
+                                    className={
+                                        route().current(item.route)
+                                            ? "font-bold"
+                                            : "font-light"
+                                    }
+                                >
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))}
+                </ul>
+            </div>
         </Container>
     );
 };
